b90fba59b9cf1e309b4547228a986781bb5aeb4e
Kyle M. Medeiros <671kmm@gmail.com>
2019-07-28 15:27:35 -0700

    kalman filter implementation, first pass

---
+++ include/ekf_slam/kalmanfilter.h
@@ -0,0 +1,51 @@
+#ifndef KALMAN_H
+#define KALMAN_H
+
+#include <eigen3/Eigen/Dense>
+class KalmanFilter
+{
+
+private:
+
+int m, n, c;
+Eigen::MatrixXd sys_dyn_A,
+                input_B,
+                output_C,
+                proc_noise_cov_Q,
+                meas_noise_cov_R,
+                est_error_cov_P,
+                kalman_gain_K,
+                init_error_cov_P0,
+                identity;
+
+Eigen::VectorXd est_state_xhat;
+
+bool is_init = false;
+
+
+public:
+  KalmanFilter(
+    const Eigen::MatrixXd &inA,
+    const Eigen::MatrixXd &inB,
+    const Eigen::MatrixXd &inC,
+    const Eigen::MatrixXd &inQ,
+    const Eigen::MatrixXd &inR,
+    const Eigen::MatrixXd &inP
+    );
+
+
+  void init();  //States = 0
+  void init(const Eigen::VectorXd &state_x0);
+
+
+  void predict_step(const Eigen::VectorXd &controls_u);
+  void update_state(const Eigen::VectorXd &meas_Y);
+  void update_dynamics_matrix(const Eigen::MatrixXd new_sys_dyn_A);
+  void update_output_matrix(const Eigen::MatrixXd new_output_C);
+
+  Eigen::VectorXd get_state();
+
+};
+
+
+#endif //KALMAN_H
---
+++ src/kalmanfilter.cpp
@@ -0,0 +1,75 @@
+#include <eigen3/Eigen/Dense>
+#include <ekf_slam/kalmanfilter.h>
+
+
+KalmanFilter::KalmanFilter(
+  const Eigen::MatrixXd &inA,
+  const Eigen::MatrixXd &inB,
+  const Eigen::MatrixXd &inC,
+  const Eigen::MatrixXd &inQ,
+  const Eigen::MatrixXd &inR,
+  const Eigen::MatrixXd &inP
+  )
+  : sys_dyn_A(inA),
+    input_B(inB),
+    output_C(inC),
+    proc_noise_cov_Q(inQ),
+    meas_noise_cov_R(inR),
+    m(output_C.rows()),
+    n(sys_dyn_A.cols()),
+    c(input_B.cols()),
+    is_init(false),
+    identity(n, n),
+    est_state_xhat(n)
+    {
+      identity.setIdentity();
+    }
+
+  void KalmanFilter::init()
+  {
+    est_state_xhat.setZero();
+    est_error_cov_P = init_error_cov_P0;
+    is_init = true;
+  }
+
+  void KalmanFilter::init(const Eigen::VectorXd &state_x0)
+  {
+    est_state_xhat = state_x0;
+    est_error_cov_P = init_error_cov_P0;
+    is_init = true;
+  }
+
+  void KalmanFilter::predict_step(const Eigen::VectorXd &controls_u)
+  {
+    if (!is_init)
+    {
+      std::cout<<"Initializing filter with estimated state zero."<<std::endl;
+      init();
+    }
+
+    est_state_xhat = sys_dyn_A * est_state_xhat + input_B * controls_u;
+    est_error_cov_P = sys_dyn_A * est_error_cov_P * sys_dyn_A.transpose() + proc_noise_cov_Q;
+  }
+
+  void KalmanFilter::update_state(const Eigen::VectorXd &meas_Y)
+  {
+    kalman_gain_K = est_error_cov_P * output_C.transpose() * (output_C * est_error_cov_P * output_C.transpose()).inverse();
+    est_state_xhat = est_state_xhat + kalman_gain_K * (meas_Y - output_C * est_state_xhat);
+    est_error_cov_P = (identity - kalman_gain_K * output_C) * est_error_cov_P;
+
+  }
+
+  void KalmanFilter::update_dynamics_matrix(const Eigen::MatrixXd new_sys_dyn_A)
+  {
+    sys_dyn_A = new_sys_dyn_A;
+  }
+
+  void KalmanFilter::update_output_matrix(const Eigen::MatrixXd new_output_C)
+  {
+    output_C = new_output_C;
+  }
+
+  Eigen::MatrixXd KalmanFilter::get_state()
+  {
+    return est_state_xhat;
+  }
