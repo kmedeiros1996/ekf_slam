97e448b96b3d0b4c72e5cd9e77a01484d8519c79
Kyle M. Medeiros <671kmm@gmail.com>
2019-08-07 20:32:07 -0700

    filled in system driver test implementation

--- src/slam_system.cpp
+++ src/slam_system.cpp
@@ -16,7 +16,36 @@ int main()
   std::vector<Eigen::Vector2d> commands = slamIO::load_commands("src/ekf_slam/resources/commands.txt");
   std::map<double, std::vector<Eigen::Vector3d>> measurements = slamIO::get_measurements("src/ekf_slam/resources/measurements.json");
 
+  EKFSlam *system = new EKFSlam(landmark_locations.size());
 
+  std::map<double, std::vector<Eigen::Vector3d>>::iterator it = measurements.begin();
 
+  size_t command_index = 0;
+  double prev_time = 0;
+  double dt = 0;
+  while (it != measurements.end())
+  {
+
+    std::cout<<"Time: "<< it->first<<std::endl<<"Measurements: "<< std::endl;
+    for (auto lm : it->second)
+      std::cout<<lm.transpose()<<",";
+
+    std::cout<<std::endl<<" Command: ";
+    std::cout<<commands[command_index].transpose()<<std::endl;
+    command_index++;
+    it++;
+
+    dt = it->first - prev_time;
+    prev_time = it->first;
+    std::cout<<"Predicting given command..."<<std::endl;
+    system->predict_step(commands[command_index], dt);
+
+    std::cout<<"Taking measurements and correcting state..."<<std::endl;
+    system->update_state(it->second);
+
+    std::cout<<"STATE: \n"<<system->get_state()<<std::endl<<"COV: \n"<<system->get_state_covariance()<<std::endl;
+  }
+
+  delete system;
   return 0;
 }
